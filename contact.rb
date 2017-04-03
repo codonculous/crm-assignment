b5347d71a74aa53037ab0cefe0d6849959d3d4b2
codonculous <feidameng01@gmail.com>
2017-04-03 00:05:38 -0400

    create methods: find_by,ful_name,delete,delete_all

--- contact.rb
+++ contact.rb
@@ -1,6 +1,6 @@
 class Contact
   @@contacts = []
-  @@id_counter = 1
+  @@id_counter = 0
   # This method should initialize the contact's attributes
   attr_accessor :first_name, :last_name, :email, :note
   attr_reader :id
@@ -27,8 +27,28 @@ class Contact
   # This method should accept an id as an argument
   # and return the contact who has that id
   def self.find(idnum)
-    @@contacts.select {|contact| contact.id == idnum}
+    @@contacts.select {|contact| contact.id == idnum.to_i}[0]
+  end
 
+  # This method should work similarly to the find method above
+  # but it should allow you to search for a contact using attributes other than id
+  # by specifying both the name of the attribute and the value
+  # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
+  def self.find_by(attribute,value)
+    @@contacts.select {|contact| contact.send(attribute.to_sym) == value}[0]
+  end
+
+  # This method should delete all of the contacts
+  def self.delete_all
+    puts "Are you sure? Y/N"
+    ans = gets.chomp
+    if ans = "Y"
+    @@contacts = []
+    end
+  end
+
+  def full_name
+    "#{@first_name} #{@last_name}"
   end
 
   # This method should allow you to specify
@@ -36,32 +56,13 @@ class Contact
   # 2. the new value for that attribute
   # and then make the appropriate change to the contact
   def update(attribute,new_value)
-
      self.send( (attribute + "=").to_sym, new_value)
-
-  end
-
-  # This method should work similarly to the find method above
-  # but it should allow you to search for a contact using attributes other than id
-  # by specifying both the name of the attribute and the value
-  # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
-  def self.find_by
-
-  end
-
-  # This method should delete all of the contacts
-  def self.delete_all
-
-  end
-
-  def full_name
-
   end
 
   # This method should delete the contact
   # HINT: Check the Array class docs for built-in methods that might be useful here
-  def delete
-
+  def delete(idnum)
+    @@contacts.delete_if {|contact| contact.id == idnum.to_i}
   end
 
   # Feel free to add other methods here, if you need them.
--- test.rb
+++ test.rb
@@ -1,7 +1,34 @@
 require_relative "contact"
 
-Contact.create("fsf","sdfsad","fadf@afdf.com","blah")
+Contact.create("fsf0","sdfsad0","fadf@afdf.com0","blah0")
+Contact.create("fsf1","sdfsad1","fadf@afdf.com1","blah1")
+Contact.create("fsf2","sdfsad2","fadf@afdf.com2","blah2")
+Contact.create("fsf3","sdfsad3","fadf@afdf.com3","blah3")
+Contact.create("fsf4","sdfsad4","fadf@afdf.com4","blah4")
+
 puts Contact.all[0].email
+puts "---------------------------------------------"
 Contact.all[0].update("email","hehe@hehe.com")
 puts Contact.all[0].email
+Contact.all[1].update("first_name","haha")
+puts Contact.all[1].first_name
+Contact.all[2].update("last_name","1111111111")
+puts Contact.all[2].last_name
+Contact.all[3].update("note","2222")
+puts Contact.all[3].note
+puts "---------------------------------------------"
+puts Contact.all[0].email
 puts Contact.all[0].id
+puts "---------------------------------------------"
+puts Contact.find("4").id
+puts Contact.find("3").note
+puts "---------------------------------------------"
+puts Contact.find_by("last_name","sdfsad4").first_name
+puts Contact.find_by("email","fadf@afdf.com3").full_name
+puts Contact.find_by("note","blah0").full_name
+puts Contact.find_by("first_name","fsf1").full_name
+puts "Contacts number #{Contact.all.length}"
+Contact.all[2].delete(2)
+puts "Contacts number #{Contact.all.length}"
+Contact.delete_all
+puts "Contacts number #{Contact.all.length}"
